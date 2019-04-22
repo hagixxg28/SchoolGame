2cb50183d96c1ffeb06a8751da29133433d89340
Hagixxg28 <hagixxg26@gmail.com>
2019-04-22 16:09:26 +0300

    Adding perk checker funciton in order to find different choices for different perks

--- src/app/components/big-test/big-test.component.ts
+++ src/app/components/big-test/big-test.component.ts
@@ -6,6 +6,10 @@ import { Bar } from 'src/app/models/bar';
 import { EventsService } from 'src/app/services/events.service';
 import { GameManagerService } from 'src/app/services/game-manager.service';
 import { trigger, state, transition, style, animate } from '@angular/animations';
+import { DayService } from 'src/app/services/day.service';
+import { Day } from 'src/app/models/day';
+import { dayTimes } from 'src/app/enums/dayTimes';
+import { Perk } from 'src/app/enums/Perks';
 
 @Component({
   selector: 'app-big-test',
@@ -17,63 +21,124 @@ import { trigger, state, transition, style, animate } from '@angular/animations'
       state('mid-high', style({ opacity: '1', background: 'linear-gradient(to right, #2c2d34, #467a5e)' })),
       state('mid-low', style({ opacity: '1', background: 'linear-gradient(to right, #2c2d34, #242424)' })),
       state('low', style({ opacity: '1', background: 'linear-gradient(to right, #2c2d34, #33ccff)' })),
-      transition('0 <=> 1', animate('1500ms ease'))
+      state('morning', style({ opacity: '1', background: 'linear-gradient(to top, #ffffcc 0%, #00ccff 100%)' })),
+      state('noon', style({ opacity: '1', background: 'linear-gradient(to top, #ffffcc 0%, #00ccff 50%)' })),
+      state('afternoon', style({ opacity: '1', background: 'linear-gradient(to top, #ff9999 28%, #0066ff 85%)' })),
+      state('evening', style({ opacity: '1', background: 'linear-gradient(to top, #ffffcc 5%, #003399 24%)' })),
+      state('night', style({ opacity: '1', background: 'linear-gradient(to right, #2980b9, #2c3e50)' })),
+      state('dream1', style({ opacity: '1', background: 'linear-gradient(to right, #1d4350, #a43931)' })),
+      state('dream2', style({ opacity: '1', background: 'linear-gradient(to right, #eecda3, #ef629f)' })),
+      state('dream3', style({ opacity: '1', background: 'linear-gradient(to right, #c33764, #1d2671)' })),
+      transition('morning<=>noon', animate('1.5s ease-in'))
     ])]
 })
 export class BigTestComponent implements OnInit {
 
-  constructor(private eventService: EventsService, private gameManager: GameManagerService) { }
+  constructor(private eventService: EventsService, private gameManager: GameManagerService, private dayService: DayService) { }
 
   ngOnInit() {
     this.Bars = this.gameManager.getFirstBars();
     this.eventService.setUpGameEvents();
-    this.pullNext();
-
+    this.eventService.eventSorter();
+    this.buildDay();
+    this.pullFromDay();
+    // this.pullNext();
   }
-
   loseBoolean: boolean = false;
-  backGroundState: String = "mid-low";
+  backGroundState: String = "morning";
   allBarsSum = 0;
-
-
+  currentDay: Day;
   Bars;
-
   Addictions = [
 
   ];
+  Perks = [Perk.Smoker];
   // SecondBars;
   event: GameEvent;
-
-
+  //#region event methods
   pullNext() {
     let pulledEvent = this.gameManager.pullNextEvent();
     this.event = pulledEvent;
   }
 
-
-
-
-
-  choicePreview(choice: Choice) {
-
-    if (!choice) {
+  pullFromDay() {
+    if (this.currentDay.morning) {
+      this.event = this.currentDay.morning
+      this.checkForPerkChoice(this.event.leftChoice)
+      this.checkForPerkChoice(this.event.rightChoice)
+      this.currentDay.morning = undefined
       return;
     }
-
-    if (choice.stressBarValue) {
-      this.previewCalculate(choice.stressBarValue, 0);
+    if (this.currentDay.noon) {
+      this.event = this.currentDay.noon
+      this.checkForPerkChoice(this.event.leftChoice)
+      this.checkForPerkChoice(this.event.rightChoice)
+      this.currentDay.noon = undefined
+      return;
     }
-
-    if (choice.socialBarValue) {
-      this.previewCalculate(choice.socialBarValue, 1)
+    if (this.currentDay.afternoon) {
+      this.event = this.currentDay.afternoon
+      this.checkForPerkChoice(this.event.leftChoice)
+      this.checkForPerkChoice(this.event.rightChoice)
+      this.currentDay.afternoon = undefined
+      return;
     }
-
-    if (choice.schoolBarValue) {
-      this.previewCalculate(choice.schoolBarValue, 2)
+    if (this.currentDay.evening) {
+      this.event = this.currentDay.evening
+      this.checkForPerkChoice(this.event.leftChoice)
+      this.checkForPerkChoice(this.event.rightChoice)
+      this.currentDay.evening = undefined
+      return;
     }
+    if (this.currentDay.night) {
+      this.event = this.currentDay.night
+      this.checkForPerkChoice(this.event.leftChoice)
+      this.checkForPerkChoice(this.event.rightChoice)
+      this.currentDay.night = undefined
+      return;
+    }
+    if (this.currentDay.dream) {
+      this.event = this.currentDay.dream
+      this.checkForPerkChoice(this.event.leftChoice)
+      this.checkForPerkChoice(this.event.rightChoice)
+      this.currentDay.dream = undefined
+      return;
+    }
+    console.log("The day is over");
+    this.buildDay();
+    this.pullFromDay();
+  }
+  buildDay() {
+    this.currentDay = new Day();
+    this.dayService.buildAndShowDay();
+    this.currentDay = this.dayService.day;
+  }
+  //#endregion
+  //#region calculation methods
+  checkForPerkChoice(choice: Choice) {
+    if (!choice.perkChoiceMap) {
+      return;
+    }
+    this.Perks.forEach(perk => {
 
-    if (choice.parentsBarValue) {
-      this.previewCalculate(choice.parentsBarValue, 3)
+      if (choice.perkChoiceMap.get(perk)) {
+        if (this.event.leftChoice === choice) {
+          this.event.leftChoice = choice.perkChoiceMap.get(perk).choice;
+          this.event.leftText = choice.perkChoiceMap.get(perk).text;
+          this.event.isPerkLeft = true;
+          return true;
+        } else {
+          this.event.rightChoice = choice.perkChoiceMap.get(perk).choice;
+          this.event.rightText = choice.perkChoiceMap.get(perk).text;
+          this.event.isPerkRight = true;
+          return true;
+        }
+      }
+    });
+    if (this.event.leftChoice === choice) {
+      this.event.isPerkLeft = false;
+    } else {
+      this.event.isPerkRight = false;
     }
 
   }
@@ -135,31 +200,18 @@ export class BigTestComponent implements OnInit {
 
     messageDelay += messageDuration;
     //Waiting for animation to end
+    if (!this.loseBoolean && !choice.nextEvent) {
+      setTimeout(() => {
+        this.pullFromDay(); this.backgroundByDay()
+      }, 1000);
+      return;
+    }
     if (!this.loseBoolean) {
       setTimeout(() => {
-        this.pullNext()
+        this.event = choice.nextEvent; this.backgroundByDay()
       }, 1000);
     }
   }
-
-
-
-
-
-  setView(calculatedValue: number, dropValue: number, index: number) {
-    this.Bars[index].value = calculatedValue;
-    this.messageShower(dropValue, index)
-    this.colorChanger(index);
-    this.calculateBarSum();
-    this.backgroundColorChanger();
-    setTimeout(() => {
-      this.messageHider(index);
-    }, 1000);
-  }
-
-
-
-
   calculate(value: number, index: number) {
     let calculatedValue = 0;
 
@@ -179,6 +231,28 @@ export class BigTestComponent implements OnInit {
     calculatedValue = this.Bars[index].value + value;
 
     return calculatedValue;
+  }
+
+  loseAlert(bar: Bar, calculatedValue) {
+    if (this.loseBoolean) {
+      return;
+    }
+
+    if (calculatedValue <= 0) {
+      this.loseBoolean = true;
+      setTimeout(() => {
+        this.event = this.gameManager.pullLoseEvent(bar, calculatedValue)
+      }, 1000);
+      return;
+    }
+    if (calculatedValue >= 100) {
+      this.loseBoolean = true;
+      setTimeout(() => {
+        this.event = this.gameManager.pullLoseEvent(bar, calculatedValue)
+      }, 1000);
+      return;
+    }
+
   }
 
   previewCalculate(value: number, index: number) {
@@ -270,88 +344,6 @@ export class BigTestComponent implements OnInit {
     }
   }
 
-  hideColor() {
-    for (let index = 0; index < this.Bars.length; index++) {
-      this.Bars[index].previewIcon = false;
-    }
-  }
-
-  messageShower(value, index) {
-    if (value > 0) {
-      this.Bars[index].showPositive = true;
-    } else {
-      this.Bars[index].showNegative = true;
-    }
-
-  }
-
-  messageHider(index) {
-    this.Bars[index].showPositive = false;
-    this.Bars[index].showNegative = false;
-  }
-
-  messagesHider() {
-    for (let index = 0; index < this.Bars.length; index++) {
-      this.Bars[index].showPositive = false;
-      this.Bars[index].showNegative = false;
-    }
-  }
-
-
-
-
-  loseAlert(bar: Bar, calculatedValue) {
-    if (this.loseBoolean) {
-      return;
-    }
-
-    if (calculatedValue <= 0) {
-      this.loseBoolean = true;
-      setTimeout(() => {
-        this.event = this.gameManager.pullLoseEvent(bar, calculatedValue)
-      }, 1000);
-      return;
-    }
-    if (calculatedValue >= 100) {
-      this.loseBoolean = true;
-      setTimeout(() => {
-        this.event = this.gameManager.pullLoseEvent(bar, calculatedValue)
-      }, 1000);
-      return;
-    }
-
-  }
-
-
-
-
-
-  colorChanger(index: number) {
-    if (this.Bars[index].value >= 25 && this.Bars[index].value <= 75) {
-      this.Bars[index].color = "primary";
-      return;
-    }
-
-    if (this.Bars[index].value < 25) {
-      this.Bars[index].color = "accent"
-      return;
-    }
-
-    if (this.Bars[index].value > 75) {
-      this.Bars[index].color = "warn"
-      return;
-    }
-
-  }
-
-  restBars() {
-    for (let index = 0; index < this.Bars.length; index++) {
-      let bar = this.Bars[index];
-      bar.value = 50;
-      this.colorChanger(index);
-    }
-  }
-
   calculateBarSum() {
     let calculatedSum = 0
     for (let index = 0; index < this.Bars.length; index++) {
@@ -361,6 +353,68 @@ export class BigTestComponent implements OnInit {
     this.allBarsSum = calculatedSum;
   }
 
+  //#endregion
+  //#region view methods
+  messageShower(value, index) {
+    if (value > 0) {
+      this.Bars[index].showPositive = true;
+    } else {
+      this.Bars[index].showNegative = true;
+    }
+
+  }
+  messageHider(index) {
+    this.Bars[index].showPositive = false;
+    this.Bars[index].showNegative = false;
+  }
+  messagesHider() {
+    for (let index = 0; index < this.Bars.length; index++) {
+      this.Bars[index].showPositive = false;
+      this.Bars[index].showNegative = false;
+    }
+  }
+  setView(calculatedValue: number, dropValue: number, index: number) {
+    this.Bars[index].value = calculatedValue;
+    this.messageShower(dropValue, index)
+    this.colorChanger(index);
+    this.calculateBarSum();
+    // this.backgroundColorChanger();
+    // this.backgroundByDay();
+    setTimeout(() => {
+      this.messageHider(index);
+    }, 1000);
+  }
+  restBars() {
+    for (let index = 0; index < this.Bars.length; index++) {
+      let bar = this.Bars[index];
+      bar.value = 50;
+      this.colorChanger(index);
+    }
+  }
+  choicePreview(choice: Choice) {
+
+    if (!choice) {
+      return;
+    }
+
+    if (choice.stressBarValue) {
+      this.previewCalculate(choice.stressBarValue, 0);
+    }
+
+    if (choice.socialBarValue) {
+      this.previewCalculate(choice.socialBarValue, 1)
+    }
+
+    if (choice.schoolBarValue) {
+      this.previewCalculate(choice.schoolBarValue, 2)
+    }
+
+    if (choice.parentsBarValue) {
+      this.previewCalculate(choice.parentsBarValue, 3)
+    }
+
+  }
+  //#region color methods
   backgroundColorChanger() {
     if (this.allBarsSum >= 0 && this.allBarsSum <= 160) {
       this.backGroundState = 'low'
@@ -382,7 +436,55 @@ export class BigTestComponent implements OnInit {
       return;
     }
   }
+  backgroundByDay() {
+    if (this.event.dayTime === dayTimes.morning) {
+      this.backGroundState = 'morning'
+      return;
+    }
+    if (this.event.dayTime === dayTimes.noon) {
+      this.backGroundState = 'noon'
+      return;
+    }
+    if (this.event.dayTime === dayTimes.afternoon) {
+      this.backGroundState = 'afternoon'
+      return;
+    }
+    if (this.event.dayTime === dayTimes.evening) {
+      this.backGroundState = 'evening'
+      return;
+    }
+    if (this.event.dayTime === dayTimes.night) {
+      this.backGroundState = 'night'
+      return;
+    }
+    if (this.event.dayTime === dayTimes.dream) {
+      this.backGroundState = 'dream1'
+      return;
+    }
+  }
+  colorChanger(index: number) {
+    if (this.Bars[index].value >= 25 && this.Bars[index].value <= 75) {
+      this.Bars[index].color = "primary";
+      return;
+    }
 
+    if (this.Bars[index].value < 25) {
+      this.Bars[index].color = "accent"
+      return;
+    }
 
+    if (this.Bars[index].value > 75) {
+      this.Bars[index].color = "warn"
+      return;
+    }
 
+  }
+  hideColor() {
+    for (let index = 0; index < this.Bars.length; index++) {
+      this.Bars[index].previewIcon = false;
+    }
+  }
+  //#endregion
+
+  //#endregion
 }
