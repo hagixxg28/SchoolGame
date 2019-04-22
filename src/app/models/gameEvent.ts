5821169861131904f99dddfef0c0c5ca300bfd14
Hagixxg28 <hagixxg26@gmail.com>
2019-04-22 16:10:30 +0300

    Adding attributes to game event

--- src/app/models/gameEvent.ts
+++ src/app/models/gameEvent.ts
@@ -3,11 +3,14 @@ import { Choice } from "./choice";
 
 export class GameEvent {
     public constructor(
-        public character:Character,
+        public character: Character,
+        public dayTime: String,
         public mainText: string,
         public leftText: string,
         public rightText: string,
         public leftChoice?: Choice,
         public rightChoice?: Choice,
+        public isPerkRight?: boolean,
+        public isPerkLeft?: boolean,
     ) { }
 }    
