44dc10946d6b45ff0f26b70ed9891f2c6abcd742
Hagixxg28 <hagixxg26@gmail.com>
2019-04-22 16:10:18 +0300

    Changin choice to contain a map of perks and choices

--- src/app/models/choice.ts
+++ src/app/models/choice.ts
@@ -1,4 +1,8 @@
 import { Addiction } from "./addiction";
+import { GameEvent } from "./gameEvent";
+import { dayTimes } from "../enums/dayTimes";
+import { Perk } from "../enums/Perks";
+import { choiceWithText } from "./choiceWithText";
 
 export class Choice {
     public constructor(
@@ -6,6 +10,9 @@ export class Choice {
         public socialBarValue?: number,
         public schoolBarValue?: number,
         public parentsBarValue?: number,
-        public addiction?: Addiction
+        public nextEvent?: GameEvent,
+        public perkChoiceMap?: Map<Perk, choiceWithText>,
+        public addiction?: Addiction,
+        public skipToTime?: dayTimes,
     ) { }
 }    
