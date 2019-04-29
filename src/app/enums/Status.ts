6c2b68f4b65d6fc442ac6806200819f88e22cf8c
Hagixxg28 <hagixxg26@gmail.com>
2019-04-29 13:39:25 +0300

    Adding status enum

---
+++ src/app/enums/Status.ts
@@ -0,0 +1,13 @@
+
+export enum Status {
+    ManOfStress = "Man of stress",
+    TopOfTheClass = "Top of the class",
+    TheLoner = "The loner",
+    MommasBoy = "Momma's boy",
+    TeachersPet = "Teacher's pet",
+    LocalCeleb = "Local Celeb",
+    Chill = "Chill",
+    DumbDumb = "Dumb dumb",
+    lilRebel = "lil' rebel",
+    Normie = "Normie",
+}
