0cb5bdf94265c6f9f037c1b275999491794d40ff
Hagixxg28 <hagixxg26@gmail.com>
2019-05-19 18:16:46 +0300

    Updating the map service

--- src/app/Data/perk-data.service.ts
+++ src/app/Data/perk-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { PerkObject } from '../models/perk';
+import { PerkObject } from '../models/perkObject';
 import { Perk } from '../enums/Perks';
 
 @Injectable({
@@ -11,9 +11,17 @@ export class PerkDataService {
 
 
   PerksMap = new Map<string, PerkObject>([
-    [Perk.Smoker, new PerkObject(Perk.Smoker, 12)],
-    [Perk.Smoker, new PerkObject(Perk.Smoker, 12)],
-    [Perk.Smoker, new PerkObject(Perk.Smoker, 12)],
-    [Perk.Smoker, new PerkObject(Perk.Smoker, 12)],
+    [Perk.Smoker, new PerkObject(Perk.Smoker, 3)],
+    [Perk.Depressed, new PerkObject(Perk.Depressed, 5)],
+    [Perk.BadStudent, new PerkObject(Perk.BadStudent, 3)],
+    [Perk.Strong, new PerkObject(Perk.Strong, 8)],
+  ])
+
+  PerkRecoverDayMap = new Map<Perk, number>([
+    [Perk.Smoker, 3],
+    [Perk.Depressed, 5],
+    [Perk.BadStudent, 3],
+    [Perk.Strong, 8],
   ])
 }
+
