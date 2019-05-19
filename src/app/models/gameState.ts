bd72e6668763aa191ca4b33e0a1f3ab4aa5bfade
Hagixxg28 <hagixxg26@gmail.com>
2019-05-19 18:16:54 +0300

    update

--- src/app/models/gameState.ts
+++ src/app/models/gameState.ts
@@ -1,11 +1,12 @@
 import { dayTimes } from "../enums/dayTimes";
 import { Perk } from "../enums/Perks";
+import { PerkObject } from "./perkObject";
 
 export class GameState {
     public constructor(
         public dayNum: number,
         public time: dayTimes,
-        public perks: Perk[],
+        public perks: PerkObject[],
         public stressVal: number,
         public socialVal: number,
         public schoolVal: number,
