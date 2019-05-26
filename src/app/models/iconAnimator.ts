9f5f3aa5603046d54c59da0170cbe90911de26ac
Hagixxg28 <hagixxg26@gmail.com>
2019-05-26 12:01:03 +0300

    new model

---
+++ src/app/models/iconAnimator.ts
@@ -0,0 +1,8 @@
+export class iconAnimator {
+    public constructor(
+        public animationBool: boolean,
+        public iconURL?: string,
+        public posX?: number,
+        public posY?: number
+    ) { }
+}    
